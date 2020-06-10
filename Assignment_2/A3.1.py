947580bae3ef6e36174763e8e59b2cc6e326c35b
serioususer <1herrkoenig@protonmail.com>
2020-06-10 15:50:01 +0200

    minor changes on tex + a3 files

---
+++ Assignment_2/A3.1.py
@@ -0,0 +1,45 @@
+from pylab import *
+from rtlsdr import *
+import numpy
+
+
+# variables
+# full frequency band
+start_freq = int(75e6)
+end_freq = int(1e9)
+
+# alt frequency band
+#start_freq = int(80e6)
+#end_freq = int(120e6)
+
+
+# rtlsdr sample rate
+sample_rate = int(2.4e6)
+
+mid_freq = int((end_freq + start_freq)/2)
+width_freq = int(end_freq-start_freq)
+
+
+# rtlsdr configuration
+sdr = RtlSdr()
+sdr.sample_rate = sample_rate
+sdr.gain = 'auto'
+sdr.bandwith = 500e3
+
+#create new np array
+all_samples = sdr.read_samples(256*1024)
+
+# scan the frequency band
+for new_freq in range(start_freq, end_freq, sample_rate*2):
+	sdr.center_freq = new_freq + sample_rate
+	samples = sdr.read_samples(256*1024)
+	all_samples = numpy.concatenate((all_samples, samples))
+
+sdr.close()
+
+# use matplotlib for PSD
+psd(all_samples, NFFT=1024, Fs=width_freq/1e6, Fc=mid_freq/1e6)
+xlabel('Frequency (MHz)')
+ylabel('Relative power (dB)')
+
+show()
